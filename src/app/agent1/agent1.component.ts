d9cc397d915f064598ddfcf4dcefb4d21981a49b
alpesh-dudhat <51983494+alpesh-dudhat@users.noreply.github.com>
2022-10-07 10:59:43 +0530

    5 files added

---
+++ src/app/agent1/agent1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agent1',
+  templateUrl: './agent1.component.html',
+  styleUrls: ['./agent1.component.css']
+})
+export class Agent1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
